06e7b9db322d7a3ab6648b683a0b98856a13fa31
Pedro Henrique <pedro.henrique@helpper.com.br>
2020-09-10 22:49:24 -0300

    feature - Finalizado os Services de Update e Delete de produtos, service de titles no componente Header e finalizado componente de tabela

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -25,11 +25,13 @@ import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
-import { ProductReadComponent } from './components/products/product-read/product-read.component';
-import { ProductRead2Component } from './components/products/product-read2/product-read2.component';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 registerLocaleData(localePt, 'pt');
 
@@ -45,6 +47,8 @@ registerLocaleData(localePt, 'pt');
     ProductCreateComponent,
     ProductReadComponent,
     ProductRead2Component,
+    ProductUpdateComponent,
+    ProductDeleteComponent,
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { HttpClient } from '@angular/common/http';
 import { Product } from './product.model';
-import { Observable } from 'rxjs';
+import { Observable, EMPTY } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -13,29 +14,55 @@ export class ProductService {
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
-  showMessage(msg: string): void {
+  showMessage(msg: string, isError: Boolean = false): void {
     this.snackBar.open(msg, 'X', {
       horizontalPosition: 'right',
       verticalPosition: 'top',
       duration: 3000,
+      panelClass: isError ? ['msg-error'] : ['msg-sucess'],
     });
   }
 
+  handleErrror(e: any): Observable<any> {
+    this.showMessage('Ocorreu um erro!', true);
+    return EMPTY;
+  }
+
   create(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.BASE_URL, product);
+    return this.http.post<Product>(this.BASE_URL, product).pipe(
+      map(obj => obj),
+      catchError(e => this.handleErrror(e))
+    );
   }
 
   read(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.BASE_URL);
+    return this.http.get<Product[]>(this.BASE_URL).pipe(
+      map(obj => obj),
+      catchError(e => this.handleErrror(e))
+    );
   }
 
-  readById(id: number): Observable<Product> {
+  readById(id: string): Observable<Product> {
     const url = `${this.BASE_URL}/${id}`;
-    return this.http.get<Product>(url);
+    return this.http.get<Product>(url).pipe(
+      map(obj => obj),
+      catchError(e => this.handleErrror(e))
+    );
   }
 
   update(product: Product): Observable<Product> {
     const url = `${this.BASE_URL}/${product.id}`;
-    return this.http.put<Product>(url, product);
+    return this.http.put<Product>(url, product).pipe(
+      map(obj => obj),
+      catchError(e => this.handleErrror(e))
+    );
+  }
+
+  delete(id: number): Observable<Product> {
+    const url = `${this.BASE_URL}/${id}`;
+    return this.http.delete<Product>(url).pipe(
+      map(obj => obj),
+      catchError(e => this.handleErrror(e))
+    );
   }
 }
